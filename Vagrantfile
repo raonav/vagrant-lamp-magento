4d7f745ffd4bfe5a3f2f23cb92fbaf5909ff99d5
Navishkar Rao <raonav@outlook.com>
2015-08-04 17:39:07 +1200

    init

---
+++ Vagrantfile
@@ -0,0 +1,19 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+  
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+
+  config.vm.synced_folder ".", "/vagrant"
+
+  config.vm.provider "virtualbox" do |vb|
+      vb.gui = false
+      vb.memory = "1024"
+  end
+
+  config.vm.provision "shell", path: "bootstrap.sh"
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update
+
+# install needed software
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+apt-get install -y apache2 php5 php5-curl php5-mcrypt mysql-server
+
+# configurations
+php5enmod mcrypt
+a2enmod rewrite
+service apache2 reload
+service apache2 restart
+
+# download magento
+wget http://www.magentocommerce.com/downloads/assets/1.9.0.1/magento-1.9.0.1.tar.gz
