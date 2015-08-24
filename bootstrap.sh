d9573b98cc2339cb97d9b6d7f9386967ff4029b4
Navishkar Rao <raonav@outlook.com>
2015-08-24 21:19:17 +1200

    n98 magerun and some refactoring

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,26 +38,47 @@ service apache2 reload
 service apache2 restart
 locale-gen en_NZ.UTF-8
 
-# installing additional tools
-cd /vagrant
-if [ ! -d /vagrant/downloads ]; then
-    mkdir downloads
-fi
-cd downloads
-## installing composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-## installing n98-magerun
-wget http://files.magerun.net/n98-magerun-latest.phar
-chmod +x ./n98-magerun-latest.phar
-mv ./n98-magerun-latest.phar /usr/local/bin/n98-magerun
-if [ -d /vagrant/downloads ]; then
-    cd /vagrant/
-    rm -r /vagrant/downloads
-fi
+# TODO: xdebug configuration
+function phpconfiguration {
+  # increase memory limit for php apache
+  sed -i '/memory_limit = 128M/c memory_limit = 1024M' /etc/php5/apache2/php.ini
+}
 
-# installing magento
-mysql -u root -proot -e "CREATE DATABASE IF NOT EXISTS magento;"
-mysql -u root -proot -e "CREATE USER magento@localhost IDENTIFIED BY 'magento';"
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON magento.* TO magento@localhost;"
-mysql -u root -proot -e "FLUSH PRIVILEGES;"
+# installing additional tools
+function installadditionaltools {
+  cd /vagrant
+  if [ ! -d /vagrant/downloads ]; then
+      mkdir downloads
+  fi
+  cd downloads
+  ## installing composer
+  curl -sS https://getcomposer.org/installer | php
+  mv composer.phar /usr/local/bin/composer
+  ## installing n98-magerun
+  wget http://files.magerun.net/n98-magerun-latest.phar
+  chmod +x ./n98-magerun-latest.phar
+  mv ./n98-magerun-latest.phar /usr/local/bin/n98-magerun
+  cd /vagrant/
+  if [ -d /vagrant/downloads ]; then
+      rm -r /vagrant/downloads
+  fi
+}
+
+# pretty self explanatory
+function installmagento {
+  # create a new database --user=magento --password=magento --dbname=magento
+  mysql -u root -proot -e "CREATE DATABASE IF NOT EXISTS $1;"
+  mysql -u root -proot -e "CREATE USER $1@localhost IDENTIFIED BY '$1';"
+  mysql -u root -proot -e "GRANT ALL PRIVILEGES ON $1.* TO $1@localhost;"
+  mysql -u root -proot -e "FLUSH PRIVILEGES;"
+
+  cd /var/www
+
+  # install magento with magerun
+  # TODO: magerun yaml configuration
+  sudo -u vagrant n98-magerun install --dbHost="localhost" --dbUser="$1" --dbPass="$1" --dbName="$1" --installSampleData=yes --useDefaultConfigParams=yes --magentoVersionByName="magento-mirror-1.9.2.1" --installationFolder="$1" --baseUrl="http://${MACHINE_NAME}/"
+}
+
+installadditionaltools
+
+installmagento magento
